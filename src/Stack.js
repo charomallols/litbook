f4653560c0c9915b6240e7b5a4ae400ea0d645dd
github_charomallols <charo.mallols@gmail.com>
2021-04-08 00:32:34 +0200

    Proyecto terminado

--- src/Stack.js
+++ src/Stack.js
@@ -6,7 +6,7 @@ export default function Stack(props) {
       <Link to={`/${props.id}`} className='item'>
         <img src={props.logo} alt={`${props.name} logo`} />
         <div className='overlay'>
-          <h1>{props.name}!</h1>
+          <h1>{props.name}</h1>
         </div>
       </Link>
     );
